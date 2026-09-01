2adc69f828aaa368415a8a9b8a6c29a83a28c830
tcfshtw <tcfshtw@gmail.com>
2026-09-01 12:20:20 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// 檔案：config.js
+const MY_LIFF_ID = "2011351908-6yan9QMs"; 
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyxmqsLVqwOt9TIZdc3nwlq3_JWTijuEgChhBmgasraHTGtxwpoEm2lL_q-QdSIUYW4Wg/exec"; 
+
+// 全域狀態變數
+let currentUserLineId = "";
+let isSuperAdminUser = false; 
+window.allEvents = []; // 暫存活動清單
