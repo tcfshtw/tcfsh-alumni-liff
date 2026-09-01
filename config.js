9fa0d2faa254b74b851eed7f18e568dc795e3e73
tcfshtw <tcfshtw@gmail.com>
2026-09-01 16:40:43 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const MY_LIFF_ID = "2011351908-6yan9QMs"; 
 const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyxmqsLVqwOt9TIZdc3nwlq3_JWTijuEgChhBmgasraHTGtxwpoEm2lL_q-QdSIUYW4Wg/exec"; 
 
-// 全域狀態變數
 let currentUserLineId = "";
 let isSuperAdminUser = false; 
-window.allEvents = []; // 暫存活動清單
+window.currentUserProfile = null;
+window.allEvents = []; 
