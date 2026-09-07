397fd3d0962cd99230cabf2aaf6fb43b4ee36601
tcfshtw <tcfshtw@gmail.com>
2026-09-07 20:59:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,5 @@ const MY_LIFF_ID = "2011351908-6yan9QMs";
 const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyxmqsLVqwOt9TIZdc3nwlq3_JWTijuEgChhBmgasraHTGtxwpoEm2lL_q-QdSIUYW4Wg/exec"; 
 
 let currentUserLineId = "";
-let isSuperAdminUser = false; 
 window.currentUserProfile = null;
 window.allEvents = []; 
