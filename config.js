8e3ccf6b008eaae0c98aee35c31cecf4f77c86a9
tcfshtw <tcfshtw@gmail.com>
2026-09-14 17:04:45 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,9 @@
 const MY_LIFF_ID = "2011351908-6yan9QMs"; 
 const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyxmqsLVqwOt9TIZdc3nwlq3_JWTijuEgChhBmgasraHTGtxwpoEm2lL_q-QdSIUYW4Wg/exec"; 
 
+// 🌟 您的專屬官方 LINE ID，前端強制擁有後台開啟權限
+const OWNER_LINE_ID = "Ue77bb71abc37fb9ba3e5ac42ed17d640";
+
 let currentUserLineId = "";
 window.currentUserProfile = null;
 window.allEvents = []; 
